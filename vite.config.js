57c74de6341858851ec6beb47cd4595d26118525
gorandamnjanovic7 <damnjanovicgoran7@gmail.com>
2026-06-20 21:41:40 +0200

    Ispos src foldera

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// POČETAK FAJLA: vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// POČETAK FUNKCIJE: Vite Konfiguracija
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5173,
+
+    // Firebase Google Auth popup fix
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+      'Cross-Origin-Embedder-Policy': 'unsafe-none'
+    }
+  }
+});
+// KRAJ FUNKCIJE: Vite Konfiguracija
+// KRAJ FAJLA: vite.config.js
